c344bae7e412bb1f2e2198f032f87044053f9cfa
Md Mukim Uddin <mdmukimuddin.bd@gmail.com>
2025-03-24 06:30:01 +0000

    new change

---
+++ src/pages/AdminPanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AdminPanel() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      {/* Admin panel content will be added here */}
+    </div>
+  );
+}
+
+export default AdminPanel;
