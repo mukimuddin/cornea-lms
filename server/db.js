ad159f2fe70703645c709ad1f6e7dc6c72f355e8
mukimuddin <mdmukimuddin.bd@gmail.com>
2025-03-25 16:20:49 +0600

    fafa

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ mongoose.set('debug', true); // Enable Mongoose debugging
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI); // Use MONGO_URI from .env
+    const conn = await mongoose.connect(process.env.MONGO_URI); // Use MONGO_URI from environment
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
