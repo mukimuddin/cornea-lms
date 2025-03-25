abf06fab9ade7b0628a6dd934b06329d4225d0f8
mukimuddin <mdmukimuddin.bd@gmail.com>
2025-03-25 16:58:46 +0600

    fafaf

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Ensure assets are resolved correctly
   plugins: [react()],
   server: {
     proxy: {
